6560972f23a8990ce0cd660116fd58ec1a42d052
bueinat <bueinat@gmail.com>
2021-12-05 12:17:57 +0200

    picking points fixed

--- controllers/PRM/krembot.ino.cpp
+++ controllers/PRM/krembot.ino.cpp
@@ -10,7 +10,8 @@ TO ASK:
 - how can I use structures that are in ARGOS documentation?
 */
 
-float radius = 0.1;
+float RADIUS = 0.1;
+int NMILESTONES = 20;
 int col, row;
 
 int **occupancyGrid;
@@ -21,8 +22,7 @@ int height, width;
 CVector2 pos;
 CDegrees degreeX;
 Real coarseResolution;
-std::map<std::pair<float, float>,CVector2> milestones;
-// std::set<CVector2> milestones;
+std::map<std::pair<float, float>, CVector2> milestones;
 
 int coraseWidth, coarseHeight;
 
@@ -30,38 +30,36 @@ enum State
 {
     move,
     turn
-} state = turn;
+} state;
 
 void PRM_controller::setup()
 {
     krembot.setup();
     krembot.Led.write(0, 255, 0);
 
+    state = turn;
     occupancyGrid = mapMsg.occupancyGrid;
     resolution = mapMsg.resolution;
     origin = mapMsg.origin;
     height = mapMsg.height;
     width = mapMsg.width;
 
-    // the size of each grid
-    coarseResolution = (float)radius;
     // relative resolution indicates the number of cells which come into one in the coarse grid
-    int relativeResolution = (int)(coarseResolution / resolution);
+    int relativeResolution = (int)(RADIUS / resolution);
 
     // create coarse grid array
-    coarseGrid = new int *[height];     // row = height
-    for (int i = 0; i < height; i++)    
+    coarseGrid = new int *[height]; // row = height
+    for (int i = 0; i < height; i++)
         coarseGrid[i] = new int[width]; // col = width
-
 
     PRM_controller::write_grid("/home/einat/grid.txt", occupancyGrid, height, width);
     PRM_controller::thickening_grid(occupancyGrid, coarseGrid, height, width, relativeResolution);
     PRM_controller::write_grid("/home/einat/coarse_grid.txt", coarseGrid, height, width);
 
     // milestones = new std::set<CVector2>();
-    
+
     // Serial.Print("--- TEST ---");
-    PRM_controller::fill_milestones_set(&milestones, height, width, 10, coarseGrid, resolution);
+    PRM_controller::fill_milestones_set(&milestones, height, width, NMILESTONES, coarseGrid);
     PRM_controller::write_grid_with_milestones("/home/einat/grid_with_ml.txt", coarseGrid, height, width);
     std::cout << "setup done! set size: " << milestones.size() << std::endl;
 }
@@ -98,25 +96,26 @@ void PRM_controller::write_grid_with_milestones(std::string filename, int **grid
     // File Open
     fid.open(filename, std::ios_base::trunc);
     int symbol;
-    
+
     // Write to the file
     for (int col = 0; col < width; col++)
     {
-        for (int row = height - 1; row >= 0; row--) {
+        for (int row = height - 1; row >= 0; row--)
+        {
             symbol = grid[col][row];
-            std::map<std::pair<float, float>,CVector2>::iterator it;
+            std::map<std::pair<float, float>, CVector2>::iterator it;
             for (it = milestones.begin(); it != milestones.end(); it++)
             {
-                // std::cout << it->first.first << ": " << it->first.second << std::endl;
-                int pCol = (it->first.first - origin.GetX() * resolution) / resolution;
-                int pRow = (it->first.second - origin.GetY() * resolution) / resolution;
-                if ((col == pCol) && (row == pRow)) {
-                                symbol = 2;
-                                break;
-                            }
+                int pCol = it->first.first / resolution;
+                int pRow = it->first.second / resolution;
+                if ((col == pCol) && (row == pRow))
+                {
+                    symbol = 2;
+                    break;
+                }
             }
             fid << symbol;
-        }      
+        }
         fid << std::endl;
     }
 
@@ -158,30 +157,32 @@ void PRM_controller::thickening_grid(int **origGrid, int **newGrid, int height, 
     }
 }
 
-static float random_float(float max) {
+static float random_float(float max)
+{
     return static_cast<float>(rand()) / static_cast<float>(RAND_MAX / max);
 }
 
-void PRM_controller::fill_milestones_set(std::map<std::pair<float, float>,CVector2>* milestones,
-                                int height, int width, int nmilestones, int** grid, Real res) {
+void PRM_controller::fill_milestones_set(std::map<std::pair<float, float>, CVector2> *milestones,
+                                         int height, int width, int nmilestones, int **grid)
+{
     std::pair<float, float> new_key;
-    while (milestones->size() < nmilestones) {
-        generate_random_point(width, height, grid, resolution, new_key);
-        if (milestones->find(new_key) == milestones->end()) {
+    while (milestones->size() < nmilestones)
+    {
+        generate_random_point(width, height, grid, new_key);
+        if (milestones->find(new_key) == milestones->end())
+        {
             milestones->insert(make_pair(new_key, CVector2(new_key.first, new_key.second)));
-            std::cout << "(" << new_key.first << ", " << new_key.second << ")" << std::endl;
+            // std::cout << "(" << new_key.first << ", " << new_key.second << ")" << std::endl;
         }
     }
-    int size = milestones->size();
-    
-    LOG << "milestones " << milestones->size() << std::endl;
-    
 }
 
-void PRM_controller::generate_random_point(int width, int height, int **grid, Real res, std::pair<float,float> &oPair) {
+void PRM_controller::generate_random_point(int width, int height, int **grid, std::pair<float, float> &oPair)
+{
     float x = random_float(width * resolution);
     float y = random_float(height * resolution);
-    while (is_point_occupied(x, y, grid, resolution) == 1) {
+    while (is_point_occupied(x, y, grid) == 1)
+    {
         x = random_float(width * resolution);
         y = random_float(height * resolution);
     }
@@ -190,14 +191,16 @@ void PRM_controller::generate_random_point(int width, int height, int **grid, Re
     oPair.second = y;
 }
 
-int PRM_controller::is_point_occupied(float x, float y, int **grid, Real res) {
+int PRM_controller::is_point_occupied(float x, float y, int **grid)
+{
     float origx = origin.GetX();
     float x_in_grid = x / resolution; // (x - origin.GetX() * res) / res;
-    float y_in_grid = y / resolution;// (y - origin.GetY() * res) / res;
+    float y_in_grid = y / resolution; // (y - origin.GetY() * res) / res;
     return grid[(int)x_in_grid][(int)y_in_grid];
 }
 
-void PRM_controller::pos_to_grid(CVector2 pos, CVector2 *grid_pos, Real res) {
+void PRM_controller::pos_to_grid(CVector2 pos, CVector2 *grid_pos, Real res)
+{
     grid_pos->SetX((pos.GetX() - origin.GetX()) / res);
     grid_pos->SetY((pos.GetY() - origin.GetY()) / res);
 }
--- controllers/PRM/krembot.ino.h
+++ controllers/PRM/krembot.ino.h
@@ -3,23 +3,26 @@
 #include <map>
 #include <tuple>
 
-struct MapMsg{
-    int ** occupancyGrid;
+struct MapMsg
+{
+    int **occupancyGrid;
     Real resolution;
     CVector2 origin;
     int height, width;
 };
 
-struct PosMsg{
+struct PosMsg
+{
     CVector2 pos;
     CDegrees degreeX;
 };
 
-
-class PRM_controller : public KrembotController {
+class PRM_controller : public KrembotController
+{
 private:
     Real robotSize = 0.20;
     bool isFirst = true;
+
 public:
     MapMsg mapMsg;
     PosMsg posMsg;
@@ -30,28 +33,31 @@ public:
     void loop();
     void write_grid(std::string filename, int **grid, int height, int width);
     void thickening_grid(int **origGrid, int **newGrid, int height, int width, Real resolution);
-    void generate_random_point(int width, int height, int **grid, Real resolution, std::pair<float,float> &oPair);
-    int is_point_occupied(float x, float y, int **grid, Real resolution);
+    void generate_random_point(int width, int height, int **grid, std::pair<float, float> &oPair);
+    int is_point_occupied(float x, float y, int **grid);
     void pos_to_grid(CVector2 pos, CVector2 *grid_pos, Real resolution);
-    void fill_milestones_set(std::map<std::pair<float, float>,CVector2>* milestones,
-                                int height, int width, int nmilestones, int** grid, Real resolution);
+    void fill_milestones_set(std::map<std::pair<float, float>, CVector2> *milestones,
+                             int height, int width, int nmilestones, int **grid);
     void write_grid_with_milestones(std::string filename, int **grid, int height, int width);
 
-    void Init(TConfigurationNode &t_node) override {
+    void Init(TConfigurationNode &t_node) override
+    {
         KrembotController::Init(t_node);
-        if ( ! krembot.isInitialized() ) {
+        if (!krembot.isInitialized())
+        {
             throw std::runtime_error("krembot.ino.cpp: krembot wasn't initialized in controller");
         }
         Particle.setName(krembot.getName());
     }
-    void ControlStep() override {
-        if(isFirst) {
+    void ControlStep() override
+    {
+        if (isFirst)
+        {
             setup();
             isFirst = false;
         }
         loop();
     }
 };
-
 
 REGISTER_CONTROLLER(PRM_controller, "PRM_controller")
